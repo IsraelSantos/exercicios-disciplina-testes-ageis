4f969c36c19bb061804f2435e2c347c6094c3795
Israel Santos <israel.santos.cr@gmail.com>
2021-05-03 12:22:28 -0300

    Alterando nome do test para executar com o comando mvn test.

--- exercicio3/src/test/java/br/uece/me/selenium/LoginUsuarioTest.java
+++ exercicio3/src/test/java/br/uece/me/selenium/LoginUsuarioTest.java
@@ -8,7 +8,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
-public class LoginUsuarioTeste {
+public class LoginUsuarioTest {
 	
 	@Test
 	public void deveEfetuarLoginCorretamente() {
